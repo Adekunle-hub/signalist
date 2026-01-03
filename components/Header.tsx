402b50d6585a770764322e29c2e74ddb451789e2
Adekunle-Hub <isholamujeebadekunle@gmail.com>
2026-01-03 19:07:47 +0100

    user image 3

--- components/Header.tsx
+++ components/Header.tsx
@@ -5,7 +5,7 @@ import UserDropdown from "./UserDropdown";
 const Header = ({ user }: { user: User }) => {
   return (
     <nav className="bg-[#141414] lg:px-16 md:px-8 px-4 py-2 sm:py-4 flex items-center justify-between">
-      <div   className="flex items-center cursor-pointer justify-center gap-2">
+      <div className="flex items-center cursor-pointer justify-center gap-2">
         <Image src="/Group(1).png" alt="Logo" width={30} height={300} />
         <span className="font-bold text-white text-xl">Signalist</span>
       </div>
