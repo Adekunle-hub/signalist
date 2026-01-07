8d3ad7e818c844ef10cf5302f4a3dc6cea44d646
Adekunle-Hub <isholamujeebadekunle@gmail.com>
2026-01-07 03:06:44 +0100

    removed .env property for inngest

--- app/api/inngest/route.ts
+++ app/api/inngest/route.ts
@@ -3,7 +3,7 @@ import { inngest } from "@/lib/inngest/client";
 import { sendDailyNewsSummary, sendSignUpEmail } from "@/lib/inngest/functions";
 
 
-export const POST = serve({
+export const {GET,POST,PUT} = serve({
   client: inngest,
   functions: [sendSignUpEmail, sendDailyNewsSummary],
 });
