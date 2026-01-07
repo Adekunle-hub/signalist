8794f1408b9e9f517d28ec59c3270f03435a5442
Adekunle-Hub <isholamujeebadekunle@gmail.com>
2026-01-07 10:56:20 +0100

    checking auth againn

--- middleware/index.ts
+++ middleware/index.ts
@@ -1,9 +1,10 @@
 import { NextRequest, NextResponse } from "next/server";
 import { headers } from "next/headers";
 
-import { auth } from "@/lib/better-auth/auth";
+import { getAuth } from "@/lib/better-auth/auth";
 
 export async function proxy(request: NextRequest) {
+  const auth = await getAuth();
   const session = await auth.api.getSession({
     headers: await headers(),
   });
@@ -18,5 +19,5 @@ export async function proxy(request: NextRequest) {
 export const config = {
   matcher: [
     "/((?!api|_next/static|_next/image|favicon.ico|sign-in|sign-up|assets)>*)",
-  ], 
+  ],
 };
