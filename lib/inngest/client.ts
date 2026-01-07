30d2aa6bc01c09bfdcd7aee5b7393c997bf2204f
Adekunle-Hub <isholamujeebadekunle@gmail.com>
2026-01-07 03:50:25 +0100

    removed .env property for inngest9

--- lib/inngest/client.ts
+++ lib/inngest/client.ts
@@ -2,7 +2,7 @@ import { Inngest } from "inngest";
 
 export const inngest = new Inngest({
   id: "signalist",
-  evenntKey: process.env.INNGEST_EVENT_KEY!,
+  eventKey: process.env.INNGEST_EVENT_KEY!,
   signingKey: process.env.INNGEST_SIGNING_KEY!,
   ai: {
     gemini: { apiKey: process.env.GEMINI_API_KEY },
